8d44e2423e6fc2290d957e288c7bd1b12c88bb16
soso <avilasofia076@gmail.com>
2022-04-29 13:14:40 -0300

    ejercicio 3 terminado

--- AYED_tps/src/practica4/ejercicio3/RecorridosAG.java
+++ AYED_tps/src/practica4/ejercicio3/RecorridosAG.java
@@ -1,7 +1,10 @@
 package practica4.ejercicio3;
 import tp04.ejercicio1.*;
+import tp02.ejercicio2.*;
 import ListasGenericas.tp02.ejercicio2.ListaEnlazadaGenerica;
 import ListasGenericas.tp02.ejercicio2.ListaGenerica;
+
+import practica2.ejercicio3.ColaGenerica;
 
 public class RecorridosAG<T> {
 	
@@ -38,7 +41,7 @@ public class RecorridosAG<T> {
 	}
 
 
-
+// cuando imprimo un general en in orden primero imprimo el mas a la izquierda, padre y luego el resto de los hijos
 	private void numerosImparesMayoresQueInOrden(Integer n, ListaEnlazadaGenerica<Integer> lis,
 			ArbolGeneral<Integer> ab) {
 		if (ab.tieneHijos()) {
@@ -56,6 +59,56 @@ public class RecorridosAG<T> {
 			}
 		}
 	}
+	
+	
+	public ListaGenerica<Integer> numerosImparesMayoresQuePostOrden(ArbolGeneral <Integer> ab, Integer n){
+		ListaEnlazadaGenerica<Integer> lis= new ListaEnlazadaGenerica<Integer>();
+		numerosImparesMayoresQuePostOrden(n,lis,ab);
+		return lis;
+	}
+	
+	private void numerosImparesMayoresQuePostOrden(Integer n, ListaEnlazadaGenerica<Integer>lis, ArbolGeneral<Integer>ab) {
+		if (ab.tieneHijos()) {
+			tp02.ejercicio2.ListaGenerica<ArbolGeneral<Integer>> lisHijos= ab.getHijos();
+			lisHijos.comenzar();
+			numerosImparesMayoresQuePostOrden(n,lis,lisHijos.proximo());			
+		}
+		if ((ab.getDato() % 2 == 1) && (ab.getDato()>n)){
+			lis.agregarFinal(ab.getDato());
+		}
+	}
+	
+	
+	public ListaGenerica<Integer> numerosImparesMayoresQuePorNiveles(ArbolGeneral <Integer> ab,Integer n){
+		ListaEnlazadaGenerica<Integer> lis= new ListaEnlazadaGenerica<Integer>();
+		ColaGenerica<ArbolGeneral<Integer>> cola= new ColaGenerica<ArbolGeneral<Integer>>();
+		ArbolGeneral<Integer> aux;
+		cola.encolar(ab);
+		cola.encolar(null);
+		
+		while (!cola.esVacia()) {
+			aux=cola.desencolar();
+			if (aux != null) {
+				if ((aux.getDato() %2 ==1) && (aux.getDato() >n)){
+					lis.agregarFinal(aux.getDato());
+				}
+				if (aux.tieneHijos()) {
+					tp02.ejercicio2.ListaGenerica<ArbolGeneral<Integer>> lisHijos= aux.getHijos();
+					lisHijos.comenzar();
+					while (!lisHijos.fin()) {
+						cola.encolar(lisHijos.proximo());
+					}
+					
+				}
+			}
+			else
+				if (!cola.esVacia()) cola.encolar(null);
+		}
+		return lis;
+	}
+	
+	
+	
 	
 	
 }
